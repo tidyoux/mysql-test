8b413a75bb91cced99102067ea6f0f713f459053
haitao <liuhaitao@artisangame.com>
2016-02-19 15:07:18 +0800

    Initial Commit

---
+++ mysql_test/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mysql_test
+//
+//  Created by haitao on 16/2/19.
+//  Copyright © 2016年 artisangame. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
